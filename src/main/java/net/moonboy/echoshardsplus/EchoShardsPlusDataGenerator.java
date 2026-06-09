6873ad50312039574bd79d4751c057274e423cc8
Justmoonboy <literarykorbyn@gmail.com>
2026-06-08 21:11:45 -0500

    Initial commit

---
+++ src/main/java/net/moonboy/echoshardsplus/EchoShardsPlusDataGenerator.java
@@ -0,0 +1,11 @@
+package net.moonboy.echoshardsplus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EchoShardsPlusDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
