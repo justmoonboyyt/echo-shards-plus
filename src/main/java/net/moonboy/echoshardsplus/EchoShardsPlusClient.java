7e471709e6eab28fe28a559ae8b0e7f66b257edc
Justmoonboy <literarykorbyn@gmail.com>
2026-06-30 21:40:11 -0500

    texture is broken but only when the mod is built

--- src/main/java/net/moonboy/echoshardsplus/EchoShardsPlusClient.java
+++ src/main/java/net/moonboy/echoshardsplus/EchoShardsPlusClient.java
@@ -1,6 +1,8 @@
 package net.moonboy.echoshardsplus;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
@@ -10,30 +12,28 @@ import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.util.Identifier;
 
-public class EchoShardsPlusClient implements ClientModInitializer{
+@Environment(EnvType.CLIENT)
+public class EchoShardsPlusClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
         ModelPredicateProviderRegistry.register(Items.CROSSBOW,
-                new Identifier("echo_shards_plus", "echo_loaded"),
-                ((stack, world, entity, seed) -> {
-                    if (!CrossbowItem.isCharged(stack))
-                        return 0.0F;
+                new Identifier(EchoShardsPlus.MOD_ID, "echo_loaded"),
+                (stack, world, entity, seed) -> {
+                    EchoShardsPlus.LOGGER.info("Predicate firing, charged:" + CrossbowItem.isCharged(stack));
+                    EchoShardsPlus.LOGGER.info("NBT:" + stack.getNbt());
+                    if (!CrossbowItem.isCharged(stack)) return 0.0f;
                     NbtCompound nbt = stack.getNbt();
-                    if (nbt==null)
-                        return 0.0F;
-                    if (!nbt.contains("ChargedProjectiles"))
-                        return 0.0F;
-
+                    if (nbt == null) return 0.0f;
+                    if (!nbt.contains("ChargedProjectiles")) return 0.0f;
                     NbtList projectiles = nbt.getList("ChargedProjectiles", NbtElement.COMPOUND_TYPE);
-
                     for (int i = 0; i < projectiles.size(); i++) {
                         ItemStack projectile = ItemStack.fromNbt(projectiles.getCompound(i));
-
-                        if (projectile.isOf(Items.ECHO_SHARD)) {
-                            return 1.0F;
-                        }
+                        EchoShardsPlus.LOGGER.info("ChargedProjectile:" + projectile.getItem());
+                        if (projectile.isOf(Items.ECHO_SHARD)) return 1.0f;
                     }
-                    return 0.0F;
-                }));
+                    return 0.0f;
+                }
+        );
     }
 }
