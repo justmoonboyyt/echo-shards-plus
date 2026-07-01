e4737e634f4ceb77495200250adfac689545353d
Justmoonboy <literarykorbyn@gmail.com>
2026-07-01 11:13:17 -0500

    texture fixed

--- src/main/java/net/moonboy/echoshardsplus/EchoShardsPlusClient.java
+++ src/main/java/net/moonboy/echoshardsplus/EchoShardsPlusClient.java
@@ -20,8 +20,6 @@ public class EchoShardsPlusClient implements ClientModInitializer {
         ModelPredicateProviderRegistry.register(Items.CROSSBOW,
                 new Identifier(EchoShardsPlus.MOD_ID, "echo_loaded"),
                 (stack, world, entity, seed) -> {
-                    EchoShardsPlus.LOGGER.info("Predicate firing, charged:" + CrossbowItem.isCharged(stack));
-                    EchoShardsPlus.LOGGER.info("NBT:" + stack.getNbt());
                     if (!CrossbowItem.isCharged(stack)) return 0.0f;
                     NbtCompound nbt = stack.getNbt();
                     if (nbt == null) return 0.0f;
@@ -29,7 +27,6 @@ public class EchoShardsPlusClient implements ClientModInitializer {
                     NbtList projectiles = nbt.getList("ChargedProjectiles", NbtElement.COMPOUND_TYPE);
                     for (int i = 0; i < projectiles.size(); i++) {
                         ItemStack projectile = ItemStack.fromNbt(projectiles.getCompound(i));
-                        EchoShardsPlus.LOGGER.info("ChargedProjectile:" + projectile.getItem());
                         if (projectile.isOf(Items.ECHO_SHARD)) return 1.0f;
                     }
                     return 0.0f;
